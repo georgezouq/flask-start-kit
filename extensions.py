7b4807fbb037d94c67eb836066a1b4c1091fb480
GeorgeZou <zousq1@lenovo.com>
2018-02-05 17:49:59 +0800

    Handle extensions library uniformity

---
+++ extensions.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+jwt = JWTManager()
+ma = Marshmallow()
