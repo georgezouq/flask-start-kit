200cf7539661dc70a9b04eff7843e435d2cd1d2c
georgezouq <zousongqi@foxmail.com>
2018-02-05 18:40:15 +0800

    Add password CryptContext

--- extensions.py
+++ extensions.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
+from passlib.context import CryptContext
 
 db = SQLAlchemy()
 jwt = JWTManager()
 ma = Marshmallow()
+pwd_context = CryptContext(schemes=['pbkdf2_sha256'], deprecated='auto')
