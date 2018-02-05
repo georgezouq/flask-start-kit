6bbf6d4802b58113dcdf7f258532b25a25f1c4ba
georgezouq <zousongqi@foxmail.com>
2018-02-05 18:39:30 +0800

    Change other way to import bluepoint

--- resources/todo/__init__.py
+++ resources/todo/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from .Todo import Todos
 from .TodoList import TodoList
 
-todo_blueprint = Blueprint('todo', __name__)
+blueprint = Blueprint('todo', __name__)
 api = Api(todo_blueprint)
 
 api.add_resource(TodoList, '/todos')
