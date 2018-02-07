027a052bdafbc99e278b8d337a5b2af062ee10a9
georgezouq <zousongqi@foxmail.com>
2018-02-07 11:54:17 +0800

    Add table init and change user's model path

--- resources/user/user.py
+++ resources/user/user.py
@@ -1,5 +1,5 @@
 from extensions import ma, db
-from models.user import User
+from models import User
 from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
