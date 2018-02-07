74145df54ef65f516c4fdbd9883a9583a43a389a
georgezouq <zousongqi@foxmail.com>
2018-02-07 11:53:21 +0800

    Change db utils add create table method

--- common/db.py
+++ common/db.py
@@ -1,28 +1,22 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
-from common.config import config
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .config import config
+from models import Base_Model
 
 engine = create_engine(
     'mysql+pymysql://' +
-    config.mysql_user + ':' +
-    config.mysql_pwd + '@' +
-    config.mysql_host + ':' +
-    config.mysql_port + '/' +
-    config.mysql_db)
+    config['mysql_user'] + ':' +
+    config['mysql_pwd'] + '@' +
+    config['mysql_host'] + ':' +
+    config['mysql_port'] + '/' +
+    config['mysql_db'])
 
-metadata = MetaData(engine)
+DB_Session = sessionmaker(bind=engine)
+session = DB_Session()
 
-# Database table structure
 
-user = Table('user', metadata,
-    Column('id', Integer,primary_key=True),
-    Column('name', String(20)),
-    Column('fullname', String(40)),
-)
-
-address_table = Table('address', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('user_id', None, ForeignKey('user.id')),
-    Column('email', String(128), nullable=False)
-)
-
-metadata.create_all()
+def init_tables():
+    print('Ready to init tables')
+    Base_Model.metadata.create_all(engine)
