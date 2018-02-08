1cd199dd4f28d4c60ee4d7e65f5a5f0ac3ee41e2
georgezouq <zousongqi@foxmail.com>
2018-02-08 10:31:44 +0800

    Add jwt secret config

--- common/config.py
+++ common/config.py
@@ -1,7 +1,10 @@
-config = {
-    'mysql_user': '',
-    'mysql_pwd': '',
-    'mysql_host': '',
-    'mysql_port': '',
-    'mysql_db': ''
+db_config = {
+    'mysql_user': 'root',
+    'mysql_pwd': 'TFMysql02',
+    'mysql_host': '127.0.0.1',
+    'mysql_port': '3306',
+    'mysql_db': 'flask_boilerplate'
 }
+
+DEBUG = True
+SECRET_KEY = "payment-platform-IKJNDFB"
